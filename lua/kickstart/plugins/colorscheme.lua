eb37cf5aa5be512fe60ecada168fa28e3d62689e
eesy-jce <codingdearth@gmail.com>
2024-08-28 11:05:08 +0300

    Kickstart Organize

---
+++ lua/kickstart/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  'rebelot/kanagawa.nvim',
+  priority = 1000,
+  init = function()
+    vim.cmd 'colorscheme kanagawa'
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      pattern = 'kanagawa',
+      callback = function()
+        if vim.o.background == 'light' then
+          vim.fn.system 'kitty +kitten themes Kanagawa_light'
+        elseif vim.o.background == 'dark' then
+          vim.fn.system 'kitty +kitten themes Kanagawa_dragon'
+        else
+          vim.fn.system 'kitty +kitten themes Kanagawa'
+        end
+      end,
+    })
+  end,
+}
